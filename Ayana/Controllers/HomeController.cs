885cd60a6a255a24cffbec27bda97769c21dc0b1
aherak2 <aherak2@etf.unsa.ba>
2023-05-16 12:15:05 +0200

    HelpView finished

--- Ayana/Controllers/HomeController.cs
+++ Ayana/Controllers/HomeController.cs
@@ -33,5 +33,10 @@ namespace Ayana.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+        public IActionResult Help()
+        {
+            return View();
+        }
+
     }
 }
